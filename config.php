9cac7941eb30b8efd91c834799daee9e3854fa2b
bigdaddydsp <bigdaddydsp@b7bae3cb-e7b7-4e78-a33b-8e47f57c2183>
2009-02-20 03:14:01 +0000

    2618501 Created configuration file and moved settings out of collection.php
    git-svn-id: https://howler.svn.sourceforge.net/svnroot/howler/branches/v0@6 b7bae3cb-e7b7-4e78-a33b-8e47f57c2183

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// File system location of the root of music.  Does not end with a slash.
+define('MUSIC_DIR', '/var/media/music');
+// URL address of root of music.  Can be absolute or relative but should be
+// accessible to the users of the system via HTTP.  Ends with a slash.
+define('MUSIC_URL', 'music/');
+?>
