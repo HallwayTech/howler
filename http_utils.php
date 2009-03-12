5fd0b2d1012032afec899bb61d99dbd50c8455ba
bigdaddydsp <bigdaddydsp@b7bae3cb-e7b7-4e78-a33b-8e47f57c2183>
2009-03-12 03:29:01 +0000

    Added http utils for getting the uri after the script name. Removed nonexistent jquery extension js file from index.html script. Refactored collection.js to use fewer local variables, simplified & clarified render methods. Added javascript doc to collection.js.
    git-svn-id: https://howler.svn.sourceforge.net/svnroot/howler/trunk@44 b7bae3cb-e7b7-4e78-a33b-8e47f57c2183

---
+++ http_utils.php
@@ -0,0 +1,21 @@
+<?php
+function http_script_uri() {
+    //$value = preg_replace('/%([0-9a-f]{2})/ie', 'chr(hexdec($1))', (string) $value);
+    // get whatever is after the name of the script
+    // add 1 to skip the first slash
+    $script_name = $_SERVER['SCRIPT_NAME'];
+    $request_uri = $_SERVER['REQUEST_URI'];
+    $len_script_name = strlen($script_name);
+    $len_request_uri = strlen($request_uri);
+
+    $uri = '';
+
+    if ($len_request_uri != $len_script_name) {
+        $uri = substr($request_uri, $len_script_name + 1);
+        $uri = str_replace("\\'", "'", $uri);
+        $uri = urldecode($uri);
+    }
+
+    return $uri;
+}
+?>
