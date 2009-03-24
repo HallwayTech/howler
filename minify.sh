a74ab8a76acf54a55b3c723520a130396d746fde
bigdaddydsp <bigdaddydsp@b7bae3cb-e7b7-4e78-a33b-8e47f57c2183>
2009-03-24 16:09:14 +0000

    Added minified versions of css.  Added updated jquery ui library.  Updated index to use minified css.  Added dev version of index that doesn't use minified files.
    git-svn-id: https://howler.svn.sourceforge.net/svnroot/howler/trunk@47 b7bae3cb-e7b7-4e78-a33b-8e47f57c2183

--- minify.sh
+++ minify.sh
@@ -1,22 +1,17 @@
+YUI_CMD="java -jar ../yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar -v"
+
 ##### minify local js files #####
 rm -rf js/*.min.js
 for f in `ls js/ | grep \.js$`; do
 	f_new="${f:0:${#f}-2}min.js"
 	echo "Minifying ${f} to ${f_new}"
-	java -jar ../yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar "js/${f}" > "js/$f_new"
+	$YUI_CMD "js/${f}" -o "js/$f_new"
 done
 
 ##### minify local css files #####
 rm -rf css/*.min.css
 for f in `ls css/ | grep \.css$`; do
-	f_new="${f:0:${#f}-2}min.css"
+	f_new="${f:0:${#f}-3}min.css"
 	echo "Minifying ${f} to ${f_new}"
-	java -jar ../yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar "css/${f}" > "css/$f_new"
+	$YUI_CMD "css/${f}" -o "css/$f_new"
 done
-
-##### minify external libraries #####
-#for f in `ls lib/ | grep \.js$`; do
-#	echo "Minifying ${f} to ${f}"
-#	java -jar ../yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar "lib/${f}" > "lib/${f}.tmp"
-#	mv "lib/${f}.tmp" "lib/$f"
-#done
