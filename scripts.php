e1806a828e776c3c62c37b37dee26ee85a2a9873
Carl F. Hall <carl.hall@gmail.com>
2009-04-13 12:04:35 -0400

    Added scripts to bundle javascript and stylesheets as 1 download each.

---
+++ scripts.php
@@ -0,0 +1,15 @@
+<?php
+//<!-- external libraries -->
+require_once("lib/jquery-1.3.2.min.js");
+require_once("lib/jquery-ui-1.7.custom.min.js");
+require_once("lib/swfobject-2.1.min.js");
+require_once("lib/trimpath-template-1.0.38.min.js");
+require_once("lib/querystring-1.3.min.js");
+require_once("lib/json2.min.js");
+//<!-- local scripts -->
+require_once("js/template.js");
+require_once("js/playlist.js");
+require_once("js/player.js");
+require_once("js/collection.js");
+require_once("js/actions.js");
+?>
---
+++ stylesheets.php
@@ -0,0 +1,5 @@
+<?php
+require_once('css/playlist.min.css');
+require_once('css/collection.min.css');
+require_once('css/player.min.css');
+?>
