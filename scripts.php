a78ee51f6d47f2d97bff16b4eb589c57d242cfaf
Carl F. Hall <carl.hall@gmail.com>
2009-04-13 12:19:30 -0400

    Corrected importing to read file straight to output buffer.

--- scripts.php
+++ scripts.php
@@ -1,15 +1,11 @@
 <?php
-//<!-- external libraries -->
-require_once("lib/jquery-1.3.2.min.js");
-require_once("lib/jquery-ui-1.7.custom.min.js");
-require_once("lib/swfobject-2.1.min.js");
-require_once("lib/trimpath-template-1.0.38.min.js");
-require_once("lib/querystring-1.3.min.js");
-require_once("lib/json2.min.js");
-//<!-- local scripts -->
-require_once("js/template.js");
-require_once("js/playlist.js");
-require_once("js/player.js");
-require_once("js/collection.js");
-require_once("js/actions.js");
+$scripts = array('lib/jquery-1.3.2.min.js', 'lib/jquery-ui-1.7.custom.min.js', 'lib/swfobject-2.1.min.js', 'lib/trimpath-template-1.0.38.min.js', 'lib/querystring-1.3.min.js', 'lib/json2.min.js', 'js/template.js', 'js/playlist.js', 'js/player.js', 'js/collection.js', 'js/actions.js');
+
+foreach ($scripts as $script) {
+    if (is_file($script)) {
+        readfile($script);
+    } else {
+        echo '// unable to read $script';
+    }
+}
 ?>
