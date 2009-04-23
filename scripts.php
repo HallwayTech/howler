426819ee0b7090a919b364db4ec7128c9c5c977f
bigdaddydsp <bigdaddydsp@b7bae3cb-e7b7-4e78-a33b-8e47f57c2183>
2009-04-23 03:30:59 +0000

    Added comments and changed include to readfile.
    git-svn-id: https://howler.svn.sourceforge.net/svnroot/howler/trunk@56 b7bae3cb-e7b7-4e78-a33b-8e47f57c2183

--- scripts.php
+++ scripts.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Stream scripts out as one file to cut down the number of connections on
+ * initial page load of the site.
+ *
+ * Note: not using include or require because they both evaluate the code.
+ */
+$stylesheets = array('css/playlist.min.css', 'css/collection.min.css', 'css/player.min.css');
 $scripts = array('lib/jquery-1.3.2.min.js', 'lib/jquery-ui-1.7.custom.min.js', 'lib/swfobject-2.1.min.js', 'lib/trimpath-template-1.0.38.min.js', 'lib/querystring-1.3.min.js', 'lib/json2.min.js', 'js/template.js', 'js/playlist.js', 'js/player.js', 'js/collection.js', 'js/actions.js');
 
 foreach ($scripts as $script) {
--- stylesheets.php
+++ stylesheets.php
@@ -1,5 +1,17 @@
 <?php
-require_once('css/playlist.min.css');
-require_once('css/collection.min.css');
-require_once('css/player.min.css');
+/**
+ * Stream stylesheets out as one file to cut down the number of connections on
+ * initial page load of the site.
+ *
+ * Note: not using include or require because they both evaluate the code.
+ */
+$stylesheets = array('css/playlist.min.css', 'css/collection.min.css', 'css/player.min.css');
+
+foreach ($stylesheets as $stylesheet) {
+    if (is_file($stylesheet)) {
+        readfile($stylesheet);
+    } else {
+        echo '// unable to read $script';
+    }
+}
 ?>
