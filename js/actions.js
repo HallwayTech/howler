196b5c5d06c7a135f384ef26a0139e2214bea549
bigdaddydsp <bigdaddydsp@b7bae3cb-e7b7-4e78-a33b-8e47f57c2183>
2009-05-28 15:41:26 +0000

    Added next button to test index recalc.
    git-svn-id: https://howler.svn.sourceforge.net/svnroot/howler/branches/v0@66 b7bae3cb-e7b7-4e78-a33b-8e47f57c2183

--- js/actions.js
+++ js/actions.js
@@ -1,5 +1,9 @@
 var Actions = function() {
 	return {
+		next: function() {
+			Playlist.controls.next();
+		},
+
 		restartPlayer: function() {
 			$('#actions-menu').attr('selectedIndex', 0);
 			Player.restart();
