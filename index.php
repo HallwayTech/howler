fb6ed18dfdcd4ca2cf5bfb7edb3901fd20712f4e
Carl F. Hall <carl.hall@gmail.com>
2009-10-12 22:03:17 -0400

    Added cakephp.

--- index.php
+++ index.php
@@ -1,56 +1,62 @@
 <?php
-require 'config.php';
-require 'bootstrap.php';
-require 'resources/ResourceController.class.php';
-
-$query = $_GET['r'];
-$resources = new ResourceController;
-
-if (!empty($query)) {
-    $resources->display($query);
-} else {
-    index();
-}
-
+/* SVN FILE: $Id$ */
 /**
- * Produces the index of the site.  Gathers all needed information and builds the
- * first view of the site without subsequent client calls to the server.
+ * Requests collector.
  *
- * @return void
+ *  This file collects requests if:
+ *	- no mod_rewrite is avilable or .htaccess files are not supported
+ *	-/public is not set as a web root.
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://www.cakephp.org)
+ * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
+ * @package       cake
+ * @since         CakePHP(tm) v 0.2.9
+ * @version       $Revision$
+ * @modifiedby    $LastChangedBy$
+ * @lastmodified  $Date$
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-function index()
-{
-    global $resources;
-    include SMARTY_DIR . 'Smarty.class.php';
+/**
+ *  Get Cake's root directory
+ */
+	define('APP_DIR', 'app');
+	define('DS', DIRECTORY_SEPARATOR);
+	define('ROOT', dirname(__FILE__));
+	define('WEBROOT_DIR', 'webroot');
+	define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
+/**
+ * This only needs to be changed if the cake installed libs are located
+ * outside of the distributed directory structure.
+ */
+	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
+		//define ('CAKE_CORE_INCLUDE_PATH', FULL PATH TO DIRECTORY WHERE CAKE CORE IS INSTALLED DO NOT ADD A TRAILING DIRECTORY SEPARATOR';
+		define('CAKE_CORE_INCLUDE_PATH', ROOT);
+	}
+	if (function_exists('ini_set')) {
+		ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS);
+		define('APP_PATH', null);
+		define('CORE_PATH', null);
+	} else {
+		define('APP_PATH', ROOT . DS . APP_DIR . DS);
+		define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+	}
+	require CORE_PATH . 'cake' . DS . 'basics.php';
+	$TIME_START = getMicrotime();
+	require CORE_PATH . 'cake' . DS . 'config' . DS . 'paths.php';
+	require LIBS . 'object.php';
+	require LIBS . 'inflector.php';
+	require LIBS . 'configure.php';
 
-    // initialize templating
-    $smarty = new Smarty;
-    $smarty->caching = CACHE_TEMPLATES;
-
-    if (!$smarty->is_cached('index.tpl')) {
-        // repeat menu list
-        $smarty->assign('repeat_values', array('NONE', 'SONG', 'LIST'));
-        $smarty->assign(
-            'repeat_output', array('Repeat None', 'Repeat Song', 'Repeat List')
-        );
-        $smarty->assign('repeat', DEFAULT_REPEAT);
-
-        // 'random' checkbox
-        $smarty->assign('random', DEFAULT_RANDOM);
-
-        // build alpha numeric navigation
-        $alphaNav= array('#');
-        for ($i = ord('a'), $size = ord('z'); $i <= $size; $i++) {
-            $alphaNav[] = chr($i);
-        }
-        $smarty->assign('alphaNav', $alphaNav);
-
-        // saved playlists
-        $playlists = array(); //$resources->dispatch('playlists');
-
-        $smarty->assign('saved_playlists', $playlists);
-    }
-
-    $smarty->display('index.tpl');
-}
+	$bootstrap = true;
+	$url = null;
+	require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
 ?>
