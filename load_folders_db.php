29cb7d80bcc756c52d34634995acca345a7280fc
Carl <carl.hall@gmail.com>
2009-11-23 23:45:43 -0500

    Added more processing to digest the music catalog.

--- load_folders_db.php
+++ load_folders_db.php
@@ -1,10 +1,37 @@
 <?php
 #!/usr/bin/php
+$root = '/home/chall/Music';
+
+function walkdir($dir) {
+	$root = '/home/chall/Music';
+	$dirs = array();
+	$files = array();
+
+	$nodes = scandir("$root/$dir");
+	foreach ($nodes as $node) {
+		if (substr($node, 0, 1) == '.') {
+			continue;
+		}
+
+		$key = sha1("$dir/$node");
+		if (is_dir("$root/$dir/$node")) {
+			$dirs[$key] = walkdir("$dir/$node");
+		} elseif (is_file($node)) {
+			$files[$key] = $node;
+		}
+	}
+
+	$retval['dirs'] = $dirs;
+	$retval['files'] = $files;
+	return $retval;
+}
 
 $cats = array();
+$dirs = array();
+$files = array();
 
 // loop over the filesystem
-$nodes = scandir('/home/chall/Music');
+$nodes = scandir($root);
 foreach ($nodes as $node) {
 	if (substr($node, 0, 1) == '.') {
 		continue;
@@ -16,5 +43,12 @@ foreach ($nodes as $node) {
 	$tcats = ($cats[$alphanum]) ? $cats[$alphanum] : array();
 	$tcats[] = sha1($node);
 	$cats[$alphanum] = $tcats;
+
+	$key = sha1($node);
+	if (is_dir("$root/$node")) {
+		$dirs[$key] = walkdir($node);
+	} elseif (is_file("$root/$node")) {
+		$files[$key] = $node;
+	}
 }
-echo json_encode($cats);
+echo json_encode($dirs);
