a6ffc39c739017cd4f1a50bac411a310a0e96323
Carl F. Hall <carl.hall@gmail.com>
2009-12-22 19:38:16 -0500

    Added onclick to content div to playlist items.

--- system/application/views/playlist_item.php
+++ system/application/views/playlist_item.php
@@ -1,15 +1,15 @@
 <li id='playlist-item-<?= $_id ?>' class='playlist-item'>
-    <div class='content'>
-        <a href="#" onclick="Player.controls.play('<?= $_id ?>');return false">&#187;</a>
+    <div class='content' onclick='Player.controls.play("<?= $_id ?>")'>
+        <a href='#' onclick='Player.controls.play("<?= $_id ?>");return false'>&#187;</a>
 <?php if ($artist or $title or $album): ?>
-        <span class="artist"><?= $artist ?></span> - <span class="title"><?= $title ?></span>
-        <span class="album"><?= $album ?></span>
+        <span class='artist'><?= $artist ?></span> - <span class='title'><?= $title ?></span>
+        <span class='album'><?= $album ?></span>
 <?php else: ?>
-        <span class="label"><?= $file ?></span>
+        <span class='label'><?= $file ?></span>
 <?php endif ?>
     </div>
     <div class='remove'>
-        <a href='#' onclick="Playlist.removeItem('<?= $_id ?>');return false">X</a>
+        <a href='#' onclick='Playlist.removeItem('<?= $_id ?>');return false'>X</a>
     </div>
     <div class='clear'></div>
 </li>
