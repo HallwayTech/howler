bd0ccb35a56f9cb943ba1d1cef51014bcc8b452a
Carl Hall <carl@hallwaytech.com>
2010-10-18 23:53:08 -0400

    Mostly formatting and some minor clean up.

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -1,7 +1,7 @@
 class EntryController < ApplicationController
     def find_all_by
         ## TODO convert this from grails to proper rails
-        def properType = params.type[0].toUpperCase() + params.type[1..-1].toLowerCase()
+        #def properType = params.type[0].toUpperCase() + params.type[1..-1].toLowerCase()
         properType = params[:type].capitalize
 
         @types = Entry.where({ params[:type] => params[params[:type]] }).order('album, track, artist')
