4cb9a94feb6aa82907d30f8ccde631b31cfff8ff
Carl Hall <carl@hallwaytech.com>
2010-11-30 00:32:20 -0500

    Dropped that rails nonsense.  Gotta pick something or this thing won't get finished.

--- test/functional/entry_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EntryControllerTest < ActionController::TestCase
-  test "should get findAllBy" do
-    get :findAllBy
-    assert_response :success
-  end
-
-end
--- test/unit/helpers/entry_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class EntryHelperTest < ActionView::TestCase
-end
