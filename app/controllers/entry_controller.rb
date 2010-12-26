f3cdefac0349a8ce13fab2f8ea17989870f890c0
Carl Hall <carl@hallwaytech.com>
2010-12-26 17:27:33 -0500

    Updated to rails 3.0.3. Added some more functionality to the controller. Added some meat to the views. Added loop for dataload script.

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -1,12 +1,70 @@
 class EntryController < ApplicationController
+    # Find entries by a field.
     def find_all_by
-        ## TODO convert this from grails to proper rails
-        #def properType = params.type[0].toUpperCase() + params.type[1..-1].toLowerCase()
-        properType = params[:type].capitalize
-
-        @types = Entry.where({ params[:type] => params[params[:type]] }).order('album, track, artist')
-#		def types = Entry."findAllBy${properType}"(params."${params.type}", params)
-#        [entries:types]
+        @entries = Entry.where({ params[:type] => params[params[:type]] }).order('album, track, artist')
+        # def properType = params.type[0].toUpperCase() + params.type[1..-1].toLowerCase()
+        # 
+        # def types = Entry.withCriteria {
+        #     cache true
+        #     eq params.type, params."${params.type}"
+        #     order 'album'
+        #     order 'track'
+        #     order 'artist'
+        # }
     end
 
+    # List all entries by a certain type (e.g. artist, album).
+    def list_by
+        # params.first = params.first ? params.int('first') : null
+        # params.max = params.max ? params.int('max') : null
+
+        @type = params[:type]
+        @types = Entry.count(:all, :conditions = "#{params[:type]} = #{params[param[:type]]}", :order = params[:type])
+        # def entries = Entry.withCriteria {
+        #     cache true
+        #     projections {
+        #         groupProperty params.type
+        #         rowCount()
+        #     }
+            # if (params.first)
+            #     firstResult(params.first)
+            # end
+            # if (params.max)
+            #     maxResults(params.max)
+            # end
+        #     order params.type
+        # }
+        # [entries: entries, type: params.type]
+    end
+
+    def stream
+        def entry = Entry.get(params[:id])
+        def file = new File(entry.path)
+        if (file.canRead())
+            response.status = 200
+            response.contentType = "audio/mpeg"
+            response.setHeader "Content-Length", "${file.length()}"
+            response.setHeader "Content-Disposition", "attachment; filename=${params.id}"
+            def os = response.outputStream
+            os << file.newInputStream()
+            os.flush()
+        else
+            response.sendError 404
+        end
+    end
+
+    def category(name)
+        def categoryMatch = name =~ /(A-Za-z0-9)/
+        def category = ''
+        (name =~ /([A-Za-z0-9])/).each do |match, group|
+            if (category == '') {
+                if (group.isNumber())
+                    category = '#'
+                else
+                    category = group.toUpperCase()
+                end
+            end
+        end
+        return category
+    end
 end
---
+++ script/dataload.rb
@@ -0,0 +1,12 @@
+require 'find'
+ 
+def find_and_print(path, pattern)
+  Find.find(path) do |entry|
+    if File.file?(entry) and entry[pattern]
+      puts entry
+    end
+  end
+end
+
+# print all the ruby files
+find_and_print(ARGV[0], /.+\.rb$/
