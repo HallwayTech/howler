a39e936a5c15482270db17dd32c1a8d2ca6bf80f
Carl Hall <carl@hallwaytech.com>
2010-12-28 23:12:23 -0500

    Added mingw as a platform. Added gem for sqlite for mingw. Trying to get link_to_remote to show correct output.

--- app/helpers/entry_helper.rb
+++ app/helpers/entry_helper.rb
@@ -5,19 +5,19 @@ module EntryHelper
   # *action*:: Action to call to. See +url_for+.
   # params:: Parameters (data) to send with the call.
   # update:: HTML entity to update. Accepts a jQuery selector.
-  def remote_function(var)
-    url = url_for(:controller => var[:controller], :action => var[:action])
+  def remote_function(args)
+    url = url_for(:controller => args[:controller], :action => args[:action])
     js = "$.get('#{url}'"
-    js += ", #{ActiveSupport::JSON.encode(var[:params])}" if var[:params]
-    js += ", function(data) {$('#{var[:update]}').html(data)}" if var[:update]
+    js += ", #{ActiveSupport::JSON.encode(args[:params])}" if args[:params]
+    js += ", function(data) {$('#{args[:update]}').html(data)}" if args[:update]
     js += ');return false'
     return js
   end
 
   # Renders an anchor link that makes a remote call using AJAX. See
   # +remote_function+.
-  def link_to_remote(var)
-    onclick = remote_function(var)
-    #link = "<a href='#' onclick='#{onclick}' title='#{var[:title]}'>${artist}</a>
+  def link_to_remote(args)
+    link = "<a href='#' onclick='#{remote_function(args)}' title='#{args[:title]}'>#{args[:text]}</a>"
+    return link
   end
 end
