c557410b27fe941b472919b685ada21146da833f
Carl Hall <carl@hallwaytech.com>
2010-12-28 22:39:55 -0500

    Added link_to_remote method.

--- app/helpers/entry_helper.rb
+++ app/helpers/entry_helper.rb
@@ -1,16 +1,23 @@
 module EntryHelper
-    # Renders JavaScript to have jQuery call to the server.
-    #
-    # *controller*:: Controller to call to. See url_for.
-    # *action*:: Action to call to. See url_for.
-    # params:: Parameters (data) to send with the call.
-    # update:: HTML entity to update. Accepts a jQuery selector.
-    def remote_function(var)
-        url = url_for(:controller => var[:controller], :action => var[:action])
-        js = "$.get('#{url}'"
-        js += ", #{ActiveSupport::JSON.encode(var[:params])}" if var[:params]
-        js += ", function(data) {$('#{var[:update]}').html(data)}" if var[:update]
-        js += ');return false'
-        return js
-    end
+  # Renders JavaScript to have jQuery call to the server.
+  #
+  # *controller*:: Controller to call to. See +url_for+.
+  # *action*:: Action to call to. See +url_for+.
+  # params:: Parameters (data) to send with the call.
+  # update:: HTML entity to update. Accepts a jQuery selector.
+  def remote_function(var)
+    url = url_for(:controller => var[:controller], :action => var[:action])
+    js = "$.get('#{url}'"
+    js += ", #{ActiveSupport::JSON.encode(var[:params])}" if var[:params]
+    js += ", function(data) {$('#{var[:update]}').html(data)}" if var[:update]
+    js += ');return false'
+    return js
+  end
+
+  # Renders an anchor link that makes a remote call using AJAX. See
+  # +remote_function+.
+  def link_to_remote(var)
+    onclick = remote_function(var)
+    #link = "<a href='#' onclick='#{onclick}' title='#{var[:title]}'>${artist}</a>
+  end
 end
